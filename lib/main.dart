7228a70c577d8535dfa307451b480b7ef64b001c
julio <sandronister@gmail.com>
2019-03-18 20:20:46 -0300

    read and save json data

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,59 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+
+void main() => runApp(App());
+
+
+class App extends StatelessWidget {
+  @override
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  List<String> _todoList = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+
+  Future<File> _getFile() async{
+    final directory = await getApplicationDocumentsDirectory();
+    return File("${directory.path}/task.json");
+  }
+
+  Future<File> _saveData() async{
+    String data = json.encode(_todoList);
+    final file = await _getFile();
+    return file.writeAsString(data);
+  }
+
+  Future<String> _readData() async{
+
+    try{
+      final file = await _getFile();
+
+      return file.readAsString();
+
+    }catch(e){
+      return null;
+    }
+  }
+
+
+}
+
